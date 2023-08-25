19b4a76d770d9517b2e77d03b714f28f79a2df15
Andre Tirado <andre@Seilys-Mac-mini.local>
2023-08-24 21:30:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-cover">
+      <div className='uppercase'>Todo</div>
+      <button className=''>
+        luna
+      </button>
+
+    </div>
+  )
+}
+
+export default App
